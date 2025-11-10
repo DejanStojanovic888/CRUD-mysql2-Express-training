9785db6efe30145acd5f4171d12c38d1396259ae
Dejan Stojanovic <zasluzujene@gmail.com>
2025-11-10 11:35:08 +0100

    Resolved conflicts by choosing local updated code

--- controllers/deleteCtrl.js
+++ controllers/deleteCtrl.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const db = require("../data_base");
 
 const deleteCtrl = async (req, res) => {
@@ -10,16 +9,3 @@ const deleteCtrl = async (req, res) => {
 };
 
 module.exports = deleteCtrl;
-=======
-const db = require('../data_base')
-
-const deleteCtrl = async (req, res) => {
-    const {id} = req.params;
-    const sql = `DELETE FROM profiles WHERE id = ${id}`
-    const deletedAcc = await db.query(sql)
-    console.log(deletedAcc)
-    res.redirect('/')
-}
-
-module.exports = deleteCtrl;
->>>>>>> 1e885f683033724d32a4eea48f6135e229f9f1ef
--- controllers/updateCtrl.js
+++ controllers/updateCtrl.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const db = require("../data_base");
 
 const updateCtrl = async (req, res) => {
@@ -10,16 +9,3 @@ const updateCtrl = async (req, res) => {
 };
 
 module.exports = updateCtrl;
-=======
-const db = require('../data_base')
-
-const updateCtrl = (req,res) => {
-    const {id, active} = req.params;
-    const sql = `UPDATE profiles SET active = ${active} WHERE id = ${id}`
-    const updatedACC = await = db.query(sql)
-    console.log('UpdatedAcc: ' + updatedACC)
-    res.redirect('/')
-}
-
-module.exports = updateCtrl;
->>>>>>> 1e885f683033724d32a4eea48f6135e229f9f1ef
